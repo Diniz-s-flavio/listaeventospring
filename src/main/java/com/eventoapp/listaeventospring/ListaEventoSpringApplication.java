29165432428071ddef946a8d1a5e3d9f7a9b54ba
Flávio Diniz de Sousa <dinizdesousaflavio@gmail.com>
2022-09-21 13:51:49 -0300

    Initial commit

---
+++ src/main/java/com/eventoapp/listaeventospring/ListaEventoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.eventoapp.listaeventospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaEventoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaEventoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eventoapp/listaeventospring/ListaEventoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventoapp.listaeventospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaEventoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
